122b6fba104aa8b58a108eba3334b14afb6c30eb
Yu <yu@example.com>
2026-09-24 13:20:46 +0800

    feat: remove vllm.old

--- vllm.old/_version.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-"""Fixed source snapshot version, independent of the AE repository history."""
-
-__version__ = version = "0.1.dev6125+g3f922b8"
-__version_tuple__ = version_tuple = (0, 1, "dev6125", "g3f922b8")
-__commit_id__ = commit_id = "3f922b8f745ac86983988ccf5c1f8412731451b2"
